13f92c1368c331a90c874e32374047638fa6901f
dishu001 <2020eeb1167@iitrpr.ac.in>
2024-03-04 23:17:02 +0530

    some changes4

--- app/settings.py
+++ app/settings.py
@@ -19,7 +19,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'corsheaders',
     'core',
     'rest_framework'
@@ -103,7 +102,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
